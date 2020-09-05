b5155f12478354ab6af8d7869e2bdc7832d8c679
yvonneonu <57523367+yvonneonu@users.noreply.github.com>
2020-09-05 14:54:35 +0100

    Update Bonus1.sql

--- Bonus1.sql
+++ Bonus1.sql
@@ -3,3 +3,18 @@ FROM Tenants
 INNER JOIN
 (SELECT TenantID FROM AptTenants GROUP BY TenantID HAVING COUNT(*) > 1) C
 ON Tenants.TenanID = C.TenantID
+
+
+
+
+
+
+SELECT BuildingNne, ISNULL(Count, 0) as 'Count'
+FROM Buildings
+LEFT JOIN
+(SELECT Apartments.BuldingID, count(*) as 'Count'
+FROM Requests INNER JOIN Apartments
+ON Requests.AptID = Apartments.AptID
+WHERE Requests.Status = 'Open'
+GROUP BY Apartments.BuildingID) ReqCounts
+ON ReqCounts.BuildingID = Buildings.BuidingID
